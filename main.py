2aa15284fd0540975c21b718e7f710349b9bedb0
Alex Yuwen <alexyuwen@Alexs-MacBook-Pro.local>
2024-02-27 21:19:50 -0700

    .

---
+++ main.py
@@ -0,0 +1,9 @@
+def func(n, k):
+    # Let notes be a sorted set of size n.  Let 0 <= k <= n - 1 be the start index.
+    # Assume a function, dist(i, j), that returns the distance between buttons i and j in O(1) time.  Note 0 <= i, j <= n - 1
+    # XXX
+
+    # DYNAMIC PROGRAMMING OR BACKTRACKING?
+
+
+    return
